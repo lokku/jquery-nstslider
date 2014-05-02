3b0c79f0d4f533d3b02cd812e98ddf83e5210167
Savio Dimatteo <darksmo@gmail.com>
2014-05-03 00:49:50 +0100

    Invert up/down keyboard behaviour

--- src/jquery.nstSlider.js
+++ src/jquery.nstSlider.js
@@ -1278,13 +1278,13 @@
 
                             switch (e.which) {
                                 case 37:   // left
-                                case 38:   // up
+                                case 40:   // down
                                     if (_is_left_grip) { nextLeft--; } else { nextRight--; }
                                     e.preventDefault();
                                     break;
 
+                                case 38:   // up
                                 case 39:   // right 
-                                case 40:   // down
                                     if (_is_left_grip) { nextLeft++; } else { nextRight++; }
 
                                     e.preventDefault();
